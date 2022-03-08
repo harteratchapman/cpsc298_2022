f521b50ad7c999967b402662629b850ac7b50cce
root <root@ip-172-31-0-21.us-west-1.compute.internal>
2022-03-08 19:45:31 +0000

    added my script

--- dylan/ec2_setup.sh
+++ dylan/ec2_setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
-yum update -y
-yum install httpd -y
-yum install git -y
-mkdir /wcode
-cd /wcode
-git clone --branch wood https://github.com/harteratchapman/cpsc298_2022.git
+sudo yum -y update
+sudo yum -y install httpd
+sudo yum -y install git 
+mkdir /dcode
+cd /dcode
+git clone --branch dylan https://github.com/harteratchapman/cpsc298_2022.git
+git push --set-upstream origin dylan
 cp cpsc298_2022/wood/html/index.html /var/www/html/index.html
 service httpd start
 chkconfig httpd on
