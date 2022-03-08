4d61350c10753373dd078252619d8426db3d4a47
root <root@ip-172-31-8-249.us-west-1.compute.internal>
2022-03-08 22:07:17 +0000

    fixed sciprt

--- dylan/ec2_setup.sh
+++ dylan/ec2_setup.sh
@@ -7,5 +7,7 @@ cd /dcode
 git clone --branch dylan https://github.com/harteratchapman/cpsc298_2022.git
 git push --set-upstream origin dylan
 cp cpsc298_2022/wood/html/index.html /var/www/html/index.html
+export da=`date`
+sed -i "s/DATE_REPLACE/${da}/g" index.html
 service httpd start
 chkconfig httpd on
