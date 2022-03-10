57eb246f761b68a75152867a6abeb0e819999e3b
Jordan Odekirk <56909068+odie2020@users.noreply.github.com>
2022-03-09 16:22:07 -0800

    Update ec2_setup.sh

--- wood/ec2_setup.sh
+++ wood/ec2_setup.sh
@@ -4,7 +4,7 @@ yum install httpd -y
 yum install git -y
 mkdir /wcode
 cd /wcode
-git clone --branch wood https://github.com/harteratchapman/cpsc298_2022.git
+git clone --branch odekirk https://github.com/harteratchapman/cpsc298_2022.git
 cp cpsc298_2022/wood/html/index.html /var/www/html/index.html
 service httpd start
 chkconfig httpd on
