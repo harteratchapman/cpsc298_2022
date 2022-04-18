37a1a1c1d9249c1b59666699d984a9f1cddeecae
William Wood Harter <wood@bittymail.com>
2022-04-17 21:45:01 -0700

    works with generic lambda with generic api gateway trigger

--- wood/iot_f/s3/react-site/src/App.js
+++ wood/iot_f/s3/react-site/src/App.js
@@ -5,9 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          This is a test for a cpsc react app
-        </p>
+        <form action="https://u0kjui3jw6.execute-api.us-west-1.amazonaws.com/default/iot_f" method="GET">
+          <input type="text" name="search_query" />
+          <button type="submit">
+            Submit
+          </button>
+        </form>
+
       </header>
     </div>
   );
