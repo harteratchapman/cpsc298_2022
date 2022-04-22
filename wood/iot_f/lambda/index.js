d203490d5e1569ff13030242f90d015586c3bd47
William Wood Harter <wood@bittymail.com>
2022-04-22 06:22:24 -0700

    A good starting point of React->ApiGateway/Lambda that does GET and POST

--- wood/iot_f/lambda/index.js
+++ wood/iot_f/lambda/index.js
@@ -1,14 +1,53 @@
 
-// working curl command
-// curl -X POST -H "Content-Type: application/json" -d '{"name":"1x"}' https://my_api_gateway_url/default/iot_f
 exports.handler = async (event) => {
-    console.log("event keys="+Object.keys(event));
-    console.log("event rrawQueryString="+event.rawQueryString);
-    console.log("event queryStringParameters="+JSON.stringify(event.queryStringParameters));
+    // this is extra handy debugging so you can see how different systems (browser, curl, api_gateway) call this function
     console.log("json = "+JSON.stringify(event));
-    var rawbody = Buffer.from(event.body, 'base64').toString('ascii');
-    console.log("decoded body = "+rawbody);
-    let body = {};
+    
+    var device_id = null;
+    
+    if (event.routeKey.endsWith('/device/{device_id}')) {
+        console.log('got a routekey ending in /device/{device_id}')
+        if (event.requestContext.http.method=='GET') {
+            if (('queryStringParameters' in event) && ('device_id' in event.queryStringParameters)) {
+                device_id = event.queryStringParameters.device_id;
+            } else if (('pathParameters' in event) && ('device_id' in event.pathParameters)) {
+                device_id = event.pathParameters.device_id
+            }
+            
+            if (device_id==null) {
+                // return the complete list of devices
+                const response = {
+                    statusCode: 200,
+                    body: JSON.stringify({'status': 'success',
+                                          'result': [{'device_id':1,'description':'the first device'},{'device_id':2,'description':'the second device'}]})
+                };
+                return response
+            } else {
+                const response = {
+                    statusCode: 200,
+                    body: JSON.stringify({'status': 'success',
+                                          'result': [{'device_id':device_id,'description':'dummy return data for device:'+device_id}]})
+                }
+                return response;
+            }
+        } else if (event.requestContext.http.method=='POST') {
+            console.log("It's a POST")
+            const response = {
+                statusCode: 200,
+                isBase64Encoded: false,
+                headers: {      
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({'status': 'success',
+                                      'result': [{'device_id':42}]})
+            };
+            console.log("responding to POST with: "+JSON.stringify(response))
+            return response;
+        }
+    } // routeKey device/{device_id}
+    
+    console.log("not a routeKey dveice: "+event.routeKey);
+    //let body = {};
     /*
     if (event.body !== null && event.body !== undefined) {
             body = JSON.parse(event.body)
@@ -17,16 +56,28 @@ exports.handler = async (event) => {
     if (event.requestContext.http.method=="POST") {
         const response = {
             statusCode: 200,
-            body: 'POST received!<br/>'+body+'<br/>keys='+Object.keys(body)
+            body: 'POST received! for endpoint: '+event.routeKey
         };
         return response 
     } else if (event.requestContext.http.method=="GET") {
         const response = {
             statusCode: 200,
-            body: 'Hello from Lambda!<br/>'+body+'<br/>keys='+Object.keys(body)
+            body: 'Hello from Lambda endpoint: '+event.routeKey
         };
         return response
-    }
+    }  else if (event.requestContext.http.method=="OPTIONS") {
+        const response = {
+            statusCode: 200,
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+            //'Access-Control-Expose-Headers': 'Content-Type,x-api-key,X-Amz-Date,Authorization,x-api-key,x-amz-security-token,Auth',
+            'Access-Control-Allow-Headers': 'x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization',
+            'Content-Type': 'application/json',
+            body: 'Options allowed: '+event.routeKey
+        };
+        console.log('returning OPTIONS')
+        return response
+    } 
 
     // Invalid response
     const response = {
--- wood/iot_f/s3/react-site/src/App.js
+++ wood/iot_f/s3/react-site/src/App.js
@@ -3,41 +3,79 @@ import {useState, useEffect, useContext} from "react";
 import logo from './logo.svg';
 
 import './App.css';
-
-const axios = import('axios');
+import * as axios from 'axios';
 
 class CpscApi {
     constructor(url) {
       this.url = url;
     }
 
-    get_device_list() {
+    get_device_list(device_id) {
+        console.log("get_device_list: "+device_id);
         const requestOptions = {
             method: 'GET',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ device_id: '1234' })
+            headers: { 'Content-Type': 'application/json' }
         };
-        axios.get(this.url)
-        .then(response => this.setState({ totalReactPackages: response.data.total }));
+        console.log("get_device_sending: "+JSON.stringify(requestOptions));
+        //const promise = axios.get(this.url+"/device/?device_id="+device_id, requestOptions);
+        const promise = axios.get(this.url+"/device/"+device_id, requestOptions);
+        const datePromise = promise.then((response) => response.data);
+
+        return datePromise;
     }
+
+    create_device(serial_number, description) {
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*" },
+        body: JSON.stringify({'serial_number': serial_number,
+                              'description': description })
+    };
+    console.log("Sending POST request to "+this.url+"/device/42");
+    const promise = axios.post(this.url+"/device/42", requestOptions);
+    const datePromise = promise.then((response) => response.data);
+    return datePromise;
+    }
+
   }
 
 
 function App() {
+  const [cpscapi, setCpscApi] = useState(null);
   const [apiUrl, setAPIUrl] = useState('https://u0kjui3jw6.execute-api.us-west-1.amazonaws.com/default/iot_f')
+  const [serialNumber, setSerialNumber] = useState('')
+  const [description, setDescription] = useState('')
 
+  const handleSerialNumberChange = event => {
+    setSerialNumber(event.target.value);
+  }
+  const handleDescriptionChange = event => {
+    setDescription(event.target.value);
+  }
+  const handleSubmit = event => {
+    event.preventDefault();
+    cpscapi.create_device(serialNumber, description).then(data => {
+      console.log('create_device done: '+ JSON.stringify(data));
+    });
+  };
+  
   useEffect(() => {
     console.log("Use effect called: " + apiUrl);
-    var cpscapi = new CpscApi(apiUrl)
+    var newcpscapi = new CpscApi(apiUrl);
+    setCpscApi(newcpscapi);
+    newcpscapi.get_device_list('939').then(data => {
+      console.log('get_device_list received: '+ JSON.stringify(data));
+    })
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <form action="https://u0kjui3jw6.execute-api.us-west-1.amazonaws.com/default/iot_f" method="POST">
-          Device ID: <input type="text" name="device_id" />
+        <form onSubmit={handleSubmit}>
+          Serial Number: <input type="text" name="serial_number" onChange={handleSerialNumberChange} value={serialNumber}/><br/>
+          Description: <input type="text" name="description" onChange={handleDescriptionChange} value={description}/>
           <button type="submit">
-            Submit
+            Create New Device
           </button>
         </form>
 
